a693ac2cd1b640c0c2251a3b50c6b10bc85bc949
raistv <54781272+raistv@users.noreply.github.com>
2020-01-17 18:28:48 +0200

    startedChar

---
+++ Home/Assets/Scripts/Character.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform Transform = GetComponent<Transform>();
+        if (Input.GetKey("d"))
+        {
+            Transform.position = Transform.position + new Vector3(1, 0, 0);
+            Debug.Log("trying to go right");
+        }
+        else if (Input.GetKey("a"))
+        {
+            Debug.Log("trying to go left");
+        }
+    }
+}
