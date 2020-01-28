2966d1464e7185d34fd0cf055201efbe2d85b844
nadav mizrahi <34983648+nadavMiz@users.noreply.github.com>
2020-01-28 20:46:33 +0200

    bbbb

---
+++ Home/Assets/Scripts/sceneLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneLoader : MonoBehaviour
+{
+
+    Character charecter;
+    Vector3 location;
+
+    void Awake()
+    {
+        if (FindObjectsOfType<sceneLoader>().Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void ChangeScene(string _sceneName)
+    {
+        if (!charecter)
+        {
+            charecter = Object.FindObjectOfType<Character>();
+        }
+        SceneManager.LoadScene(_sceneName);
+    }
+}
